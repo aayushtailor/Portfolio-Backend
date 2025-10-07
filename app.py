d69a82d5bbd2f46d535e20b15a9257ce608a1d75
Aayush Tailor <aayushtailor16@gmail.com>
2025-10-07 20:13:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_mail import Mail, Message
 import os
+from flask_cors import CORS
 
+load_dotenv()
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+
+CORS(app, resources={r"/*": {"origins": "https://aayush-devportfolio.vercel.app"}})
 
 
 # Email configuration (use your Gmail here)
@@ -44,4 +47,3 @@ def home():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
-
