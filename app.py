526b48e7f983adf19ac61d06b63d2b7defaa39a1
Aayush Tailor <aayushtailor16@gmail.com>
2025-10-07 18:57:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def home():
     return jsonify({"status": "Flask backend running!"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
